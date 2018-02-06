52adce09b1f8b361d1fd6d9b8027e03de33a6fc4
KyleMondina <kylemondina@gmail.com>
2018-02-05 22:29:43 -0800

    im too crazy to know what happened lolz xd

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ const mongoose = require('mongoose');
 const routes = require('./router.js');
 
 mongoose.connect('mongodb://weebo:weebo@ds225608.mlab.com:25608/animesearch');
-const animeSchema = new mongoose.Schema({
-  name: String,
-  imageurl: String
-});
-const AnimeModel = mongoose.model('animeCollection', animeSchema);
-module.exports = AnimeModel;
-
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
